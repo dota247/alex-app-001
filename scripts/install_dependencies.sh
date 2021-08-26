f81ba1265a9ca6d8b883acc1b12741e8b0ca7a15
Wagner, J <35028309+dota247@users.noreply.github.com>
2021-08-26 13:48:36 -0700

    Add files via upload

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 sudo yum upgrade -y
 sudo yum install curl -y
-sudo curl --silent --location https://rpm.nodesource.com/setup_14.x | sudo bash -
-sudo yum install nodejs -y
-npm install --save /home/ec2-user
+mkdir -p /home/ec2-user/app
+aws s3 cp s3://001-alex-statlogger-s3/mywebapp.zip /home/ec2-user/app/app.zip
+unzip /home/ec2-user/app/app.zip -d /home/ec2-user/app
+sudo bash /home/ec2-user/app/nodejs_14
+npm install --save /home/ec2-user/app
 
