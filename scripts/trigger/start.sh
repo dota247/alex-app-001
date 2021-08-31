d74fed57ad3c903e1b8e6c865434953f56ad5e66
Wagner, J <35028309+dota247@users.noreply.github.com>
2021-08-31 10:13:40 -0700

    Delete scripts/trigger directory

--- scripts/trigger/start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#npm --prefix /home/ec2-user/app run start
-#su - ec2-user -c 'at now +1 minutes -f /home/ec2-user/app/at/start.sh'
-su - ec2-user -c 'cd /home/ec2-user/app && npm start'
